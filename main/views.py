0377f5365f8b626dd085f0d14524a48527f835e2
roansong <roansong@gmail.com>
2017-07-22 11:15:13 +0200

    Email is now username
    Created a RegistrationForm class that requires the username to be an
    email address

--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from main.forms import RegistrationForm
 # Create your views here.
 
 def index(request):
@@ -11,3 +14,21 @@ class IndexView(TemplateView):
 
 class ResultsView(TemplateView):	
 	template_name = "main/results.html"
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            if(user is not None):
+	            login(request, user)
+
+	            return redirect('main:results')
+            else:
+            	return redirect('main:index')
+    else:
+        form = RegistrationForm()
+    return render(request, 'main/register.html', {'form': form})
--- weatherapp/urls.py
+++ weatherapp/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-
+from main import views as core_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^',include('main.urls')),
     url(r'^login/$',auth_views.login,{'template_name': 'main/login.html'},name='login'),
-    url(r'^logout/$',auth_views.logout,{'template_name': 'main/index.html'},name='logout'),
+    url(r'^logout/$',auth_views.logout,{'next_page': '/'},name='logout'),
+    url(r'^register/$',core_views.register,name='register'),
 ]
